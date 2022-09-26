8478ca2fa3cf74c7a70ad414d6a3b5df69695837
gersen.e.a <gersen.e.a@gmail.com>
2022-09-26 21:52:06 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExamExample
+{
+    internal class Program
+    {
+        static string path = @"C:\Users\ГерценЕ\source\repos\MyData.db";
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
